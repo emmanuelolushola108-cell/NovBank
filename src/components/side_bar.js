2a68a4c9bd57cae95b8ccdbe5096cec58cd5681c
advanceJs <emmanuelolushola108@gmail.com>
2026-05-11 12:19:59 +0100

    Nova bank landing page creation

---
+++ src/components/side_bar.js
@@ -0,0 +1,19 @@
+const sideBar = function () {
+  const nav = `<nav class="navigation">
+        <div class="logo_con">
+          <img src="assets/images/logo.png" alt="" class="logo" />
+        </div>
+        <ul class="nav_links">
+          <li><a href="#" class="nav_link">Home</a></li>
+          <li><a href="#" class="nav_link">Features</a></li>
+          <li><a href="#" class="nav_link">How it Works</a></li>
+          <li><a href="#" class="nav_link">About us</a></li>
+          <li><a href="#" class="nav_link">Security</a></li>
+          <li><a href="#" class="nav_link">Contact</a></li>
+        </ul>
+        <div class="left_nav">
+          <button class="btn">Login</button>
+          <button class="btn">Open Account</button>
+        </div>
+      </nav>`;
+};
