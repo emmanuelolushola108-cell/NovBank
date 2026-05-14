65a84926ffc0cea0ccfecd3c7fca1b1188a0e03e
advanceJs <emmanuelolushola108@gmail.com>
2026-05-14 12:54:39 +0100

    switching from login to signup ui implementation

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -1,5 +1,15 @@
-const signUpBtn = document.querySelector(".open_acc_btn");
-signUpBtn.addEventListener("click", (e) => {
+const signUpBtns = document.querySelectorAll(".open_acc_btn");
+const signUpLink = document.querySelector(".sign_up_link");
+const signUpPage = document.querySelector(".signup_flex");
+const loginPage = document.querySelector(".login_flex");
+signUpBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "auth.html";
+  });
+});
+signUpLink.addEventListener("click", (e) => {
   e.preventDefault();
-  window.location.href = "auth.html";
+  loginPage.classList.add("remove_login");
+  signUpPage.classList.add("sign_up");
 });
